a9c95a73015619b399e603458a3aff0ab5abcbc4
AswinAS-308247 <Aswin.AS2@ust.com>
2026-06-10 10:38:46 +0530

    Updated for Sprint 2

--- HMS_BACKEND/src/controllers/dashboardController.js
+++ HMS_BACKEND/src/controllers/dashboardController.js
@@ -5,7 +5,7 @@ const Appointments = require("../models/Appointments");
 exports.getDashboardStats = async (req, res) => {
   try {
     const totalEmployees = await Employees.countDocuments();
-    const activeEmployees = await Employees.countDocuments({ status: true });
+    const activeEmployees = await Employees.countDocuments({ status: "ACTIVE" });
     const pendingApprovals = await Users.countDocuments({
       status: "ADMIN_APPROVAL_PENDING",
     });
