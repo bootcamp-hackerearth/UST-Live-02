680954eedd8f4ae474240b193cc382d5e9d30bbc
rabjot-dev <308236@ust.com>
2026-06-13 12:19:02 +0530

    Sprint-3 Commit

---
+++ HMS_Backend/src/services/appointment/update-my-appointment.service.js
@@ -0,0 +1,145 @@
+const Appointment = require("../../models/Appointment");
+
+const updateMyAppointment = async (
+  appointmentId,
+  patientId,
+  updateData
+) => {
+
+  const {
+    appointmentDate,
+    appointmentTime,
+    symptoms,
+  } = updateData;
+
+  const appointment =
+    await Appointment.findById(
+      appointmentId
+    );
+
+  if (!appointment) {
+    throw new Error(
+      "Appointment not found"
+    );
+  }
+
+  if (
+    appointment.patientId.toString() !==
+    patientId
+  ) {
+    throw new Error(
+      "Unauthorized access"
+    );
+  }
+
+  if (
+    appointment.status !==
+    "PENDING"
+  ) {
+    throw new Error(
+      "Only pending appointments can be modified"
+    );
+  }
+
+  const selectedDate =
+    new Date(
+      appointmentDate
+    );
+
+  const today =
+    new Date();
+
+  today.setHours(
+    0, 0, 0, 0
+  );
+
+  selectedDate.setHours(
+    0, 0, 0, 0
+  );
+
+  if (
+    selectedDate < today
+  ) {
+    throw new Error(
+      "Past date not allowed"
+    );
+  }
+
+  const [year, month, day] =
+    appointmentDate
+      .split("-")
+      .map(Number);
+
+  const normalizedDate =
+    new Date(
+      year,
+      month - 1,
+      day,
+      12,
+      0,
+      0
+    );
+
+  const nextDay =
+    new Date(
+      normalizedDate
+    );
+
+  nextDay.setDate(
+    nextDay.getDate() + 1
+  );
+
+  const existingAppointment =
+    await Appointment.findOne({
+
+      _id: {
+        $ne: appointmentId,
+      },
+
+      doctorEmployeeId:
+        appointment.doctorEmployeeId,
+
+      timeSlot:
+        appointmentTime,
+
+      appointmentDate: {
+        $gte:
+          normalizedDate,
+
+        $lt:
+          nextDay,
+      },
+
+      status: {
+        $nin: [
+          "CANCELLED",
+          "REJECTED",
+          "NO_SHOW",
+        ],
+      },
+    });
+
+  if (
+    existingAppointment
+  ) {
+    throw new Error(
+      "Selected slot already booked"
+    );
+  }
+
+  appointment.appointmentDate =
+    appointmentDate;
+
+  appointment.timeSlot =
+    appointmentTime;
+
+  appointment.symptoms =
+    symptoms || [];
+
+  await appointment.save();
+
+  return appointment;
+};
+
+module.exports =
+  updateMyAppointment;
--- HMS_Backend/src/services/auth/login.service.js
+++ HMS_Backend/src/services/auth/login.service.js
@@ -2,8 +2,8 @@ const bcrypt = require("bcryptjs");
 
 const User = require("../../models/User");
 const Employee = require("../../models/Employee");
-
-const generateToken = require("../../utils/generateToken");
+const generateAccessToken = require("../../utils/generateAccessToken");
+const generateRefreshToken = require("../../utils/generateRefreshToken");
 
 const loginUser = async (loginData) => {
   const { loginId, password } = loginData;
@@ -73,20 +73,26 @@ const loginUser = async (loginData) => {
   const tokenPayload = {
     userId: user._id,
     employeeId: user.employeeId,
+    patientId: user.patientId,
     roles: user.roles,
   };
 
-  const token = generateToken(tokenPayload);
+const accessToken = generateAccessToken(tokenPayload);
 
-  // Update last login timestamp
-  user.lastLoginAt = new Date();
+const refreshToken = generateRefreshToken(tokenPayload);
 
-  await user.save();
+// Save refresh token in DB
+user.refreshToken = refreshToken;
 
-  return {
-    token,
-    user,
-  };
+user.lastLoginAt = new Date();
+
+await user.save();
+
+return {
+  accessToken,
+  refreshToken,
+  user
+};
 };
 
 module.exports = loginUser;
